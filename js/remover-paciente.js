e24f78dca0a569be048e2b98cb3936cad1cc1b8b
Wagner.Araujo <araujowp@gmail>
2022-02-25 07:22:56 -0300

    animando delete

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -5,6 +5,15 @@ var tabela  = document.querySelector("table");
 tabela.addEventListener("dblclick",function(event){
     var alvoEvento = event.target;
     var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover 
-    paiDoAlvo.remove();
+    
+    if(event.target.tagName == "TD"){
+        paiDoAlvo.classList.add("fadeOut");
+
+        setTimeout(function(){
+            paiDoAlvo.remove();
+        },500);
+    }
+
+
     // event.target.parentNode.remove(); //podemos fazer em uma linda mas este é um exemplo didaticos
 });
